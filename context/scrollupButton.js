861a47187ea8d3a0d6287ea73fcfc9b411346073
renzy.code <125983459+renzy-code@users.noreply.github.com>
2024-08-17 10:50:17 +0800

    add bootstrap

---
+++ context/scrollupButton.js
@@ -0,0 +1,16 @@
+let scrollToTopBtn = document.getElementById("scrollToTopBtn");
+window.onscroll = function() {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollToTopBtn.style.display = "block";
+    } else {
+        scrollToTopBtn.style.display = "none";
+    }
+}
+
+scrollToTopBtn.addEventListener("click", function() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+});
